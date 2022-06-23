4d6d5efaa6b7ba4ea4e6a255d88ba0df32b07a81
marc1 <m@marc.rs>
2022-06-23 14:56:34 -0700

    refactor: move Card, Rank, Suit logic into 'deck' module

--- src/lib.rs
+++ src/lib.rs
@@ -1,59 +1,9 @@
-use snafu::{prelude::*, Whatever};
-use std::convert::{From, TryFrom};
-
-#[derive(Copy, Clone, Debug)]
-#[repr(u8)]
-enum Rank {
-    Two,
-    Three,
-    King,
-    Ace,
-}
-
-impl From<Rank> for u8 {
-    fn from(rank: Rank) -> Self {
-        rank as Self
-    }
-}
-
-#[derive(Copy, Clone, Debug)]
-#[repr(u8)]
-enum Suit {
-    Spade,
-    Heart,
-    Diamond,
-    Club,
-}
-
-impl From<Suit> for u8 {
-    fn from(suit: Suit) -> Self {
-        suit as Self
-    }
-}
-
-#[derive(Copy, Clone, Debug)]
-#[repr(transparent)]
-struct Card(u8);
-
-impl Card {
-    pub fn new(rank: Rank, suit: Suit) -> Self {
-	Self(u8::from(rank) | (u8::from(suit) << 4))
-    }
-
-    pub fn rank(self) -> Rank {
-	unsafe { std::mem::transmute(self.0 & 0b1111) }
-    }
-
-    pub fn suit(self) -> Suit {
-	unsafe { std::mem::transmute(self.0 >> 4) }
-    }
-}
+mod deck;
 
 #[cfg(test)]
 mod tests {
-
     #[test]
     fn foo() {
-        use crate::*;
+        use crate::deck::*;
     }
 }
