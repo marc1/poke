5ab9b81d722cf3c8c57633b22ddcb8c1c2823afe
marc1 <m@marc.rs>
2022-07-18 23:18:14 -0700

    test: add tests for 'deck' module

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,42 @@ mod deck;
 
 #[cfg(test)]
 mod tests {
+    use crate::deck::*;
+    use snafu::{prelude::*, Whatever};
+
     #[test]
-    fn foo() {
-        use crate::deck::*;
+    fn card_cmp() {
+	let js = Card::new(Rank::Jack, Suit::Spade);
+	let ts = Card::new(Rank::Ten, Suit::Spade);
+
+	let jh = Card::new(Rank::Jack, Suit::Heart);
+
+	assert!(js > ts);
+	assert!(jh > ts);
+	assert!(js > jh);
+    }
+
+    #[test]
+    fn suit_cmp() {
+	assert!(Suit::Spade > Suit::Club)
+    }
+        
+    #[test]
+    fn foo() -> Result<(), Whatever> {
+	use std::str::FromStr;
+
+	let mut board = CardSet::new();
+	board.inner.insert("Js".parse()?);
+	board.inner.insert("Th".parse()?);
+	board.inner.insert("7d".parse()?);
+	board.inner.insert("9s".parse()?);
+	board.inner.insert("Tc".parse()?);
+	board.inner.insert("Ah".parse()?);
+	
+	for c in board.inner.iter().rev() {
+	    println!("{}", c);
+	}
+
+	Ok(())
     }
 }
